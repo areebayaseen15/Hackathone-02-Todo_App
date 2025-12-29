a6b773f22bbab821b90be310ef47b5683ff8c50d
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-12-29 17:59:54 +0500

    Delete Phase-01 directory

--- Phase-01/src/todo/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Todo Application - Phase I: In-Memory Console App"""
-
-__version__ = "1.0.0"
--- Phase-01/src/todo/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Allow running the package with python -m todo."""
-
-from todo.main import main
-
-if __name__ == "__main__":
-    main()
